1d60c670f8bf62791a76abdd001d0d622e29818a
sanbedoyau <sanbedoya@unal.edu.co>
2025-06-18 12:37:11 -0500

    UPDATE: Varios
    - Nombre de imagenes
    - Hook para interacciones fuera de desplegables
    - Navbar

---
+++ frontend-dentalog/src/components/Navbar/Navbar.tsx
@@ -0,0 +1,11 @@
+import ZDLogo from '../../assets/ZD Logo.png';
+
+export default function Navbar() {
+    return (
+        <>
+            <nav id='navbar'>
+                <img className=''/>
+            </nav>
+        </>
+    )
+}
