a8c71f36e85cf94f260a517372aec30a4a394d38
Altraoz <curresty@unal.edu.co>
2025-06-22 21:49:17 -0500

    Static info about FAQ section
    uwu

---
+++ frontend-dentalog/src/Pages/FAQPage.tsx
@@ -0,0 +1,11 @@
+import Navbar from '../components/Navbar/Navbar';
+import FAQ from '../components/FAQ/FAQ';    
+
+export default function FAQPage() {
+    return (
+        <>
+            <Navbar />
+            <FAQ />
+        </>
+    );
+}
