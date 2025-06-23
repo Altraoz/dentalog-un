07a09b497f21d79353ce888d89df18031c6f7f27
sanbedoyau <sanbedoya@unal.edu.co>
2025-06-22 20:58:40 -0500

    ADD: AboutUs Section

--- frontend-dentalog/src/Pages/MainPage.tsx
+++ frontend-dentalog/src/Pages/MainPage.tsx
@@ -1,9 +1,11 @@
 import Navbar from '../components/Navbar/Navbar';
+import AboutUs from '../components/AboutUs/AboutUs';
 
 export default function MainPage() {
     return (
         <>
             <Navbar />
+            <AboutUs />
         </>
     );
 }
--- frontend-dentalog/src/components/Navbar/Navbar.tsx
+++ frontend-dentalog/src/components/Navbar/Navbar.tsx
@@ -1,16 +1,9 @@
 import { Link } from 'react-router-dom';
-import { useState } from 'react';
 import ZDLogo from '../../assets/ZD Logo.png';
-import useOutsideInteraction from '../hooks/useOutsideInteraction';
 import Options from './Options';
 import './Navbar.css';
 
 export default function Navbar() {
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-    const menuRef = useOutsideInteraction<HTMLDivElement>(
-        isMenuOpen,
-        () => setIsMenuOpen(false)
-    );
 
     return (
         <>
