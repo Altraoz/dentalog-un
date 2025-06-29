6c1d0a34b3d0601d2477826eea66220ee4de31a4
Altraoz <curresty@unal.edu.co>
2025-06-28 23:49:06 -0500

    xd

--- backend-dentalog/auth/settings.py
+++ backend-dentalog/auth/settings.py
@@ -15,8 +15,10 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = [
+    'django-env.eba-3ppwu5a9.us-west-2.elasticbeanstalk.com',
+    '.elasticbeanstalk.com'
+]
 
 # Application definition
 
--- backend-dentalog/auth/urls.py
+++ backend-dentalog/auth/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from knox import views as knox_views
+from django.http import HttpResponse
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -10,5 +12,8 @@ urlpatterns = [
 
     path('logout/',knox_views.LogoutView.as_view(), name='knox_logout'), 
     path('logoutall/',knox_views.LogoutAllView.as_view(), name='knox_logoutall'), 
-    path('api/password_reset/',include('django_rest_passwordreset.urls', namespace='password_reset')), 
+    path('api/password_reset/',include('django_rest_passwordreset.urls', namespace='password_reset')),
+
+    path('', lambda request: HttpResponse(status=200)),
+
 ]
