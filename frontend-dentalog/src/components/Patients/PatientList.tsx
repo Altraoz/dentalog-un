24c467204fd22239309c6ffb0fa367edb04d2319
Altraoz <curresty@unal.edu.co>
2025-07-23 02:09:18 -0500

    enhancing patient list

--- frontend-dentalog/src/components/Patients/PatientList.tsx
+++ frontend-dentalog/src/components/Patients/PatientList.tsx
@@ -8,7 +8,7 @@ import type { Patient } from "../../types";
 import "./PatientList.css";
 import { useAuth } from "../../contexts/AuthContext";
 import { getPatients } from "../../api/patients";
-
+import { CircularProgress } from "@mui/material";
 
 export const PatientList: React.FC = () => {
   const [searchTerm, setSearchTerm] = useState("");
@@ -16,19 +16,23 @@ export const PatientList: React.FC = () => {
   const [selectedPatient, setSelectedPatient] = useState<Patient | null>(null);
   const [patients, setPatients] = useState<Patient[]>([]);
   const { user } = useAuth();
+  const [waitingResponse, setWaitingResponse] = useState<boolean>(false);
 
   useEffect(() => {
     const fetchPatients = async () => {
+      setWaitingResponse(true);
+
       if (!user) {
         console.error("User is not authenticated.");
         return;
       }
       const response = await getPatients(user?.token);
       if (response?.status == 200) {
-        setPatients(response.data.results); 
-        console.log("setting patients", response)
+        setWaitingResponse(false);
+        setPatients(response.data.results);
+        console.log("setting patients", response);
 
-        setPatients(response.data.results); 
+        setPatients(response.data.results);
       } else {
         console.log("f trayendo pacientes");
       }
@@ -96,58 +100,87 @@ export const PatientList: React.FC = () => {
           />
         </div>
 
-        <div className="patient-grid">
-          {filteredPatients.map((patient) => (
+        <div>{/* waitingResponse */}</div>
+        <div
+          className="patient-grid"
+          style={
+            waitingResponse
+              ? {
+                  minHeight: "50vh",
+                  gridTemplateColumns: "repeat(1, 1fr)",
+                  placeItems: "center",
+                }
+              : {}
+          }
+        >
+          {waitingResponse ? (
             <div
-              key={patient.id}
-              onClick={() => handlePatientClick(patient)}
-              className="patient-card"
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                gap: "2rem",
+                alignItems: "center",
+              }}
             >
-              <div className="patient-card-content">
-                <div className="patient-avatar">
-                  {patient.first_name.charAt(0)}
-                  {patient.last_name.charAt(0)}
-                </div>
-
-                <div className="patient-details">
-                  <h3 className="patient-name">
-                    {patient.first_name} {patient.last_name}
-                  </h3>
-                  <p className="patient-info">
-                    {calculateAge(patient.birth_date)} años •{" "}
-                    {patient.gender === "Masculino" ? "Niño" : "Niña"}
-                  </p>
-
-                  <div className="patient-meta">
-                    <div className="meta-item">
-                      <User className="meta-icon" />
-                      <span>
-                        {patient.responsable_user.first_name}{" "}
-                        {patient.responsable_user.last_name}
-                      </span>
+              <CircularProgress sx={{ color: "##2563eb" }} />
+              <p>Cargando pacientes...</p>
+            </div>
+          ) : (
+            <>
+              {" "}
+              {filteredPatients.map((patient) => (
+                <div
+                  key={patient.id}
+                  onClick={() => handlePatientClick(patient)}
+                  className="patient-card"
+                >
+                  <div className="patient-card-content">
+                    <div className="patient-avatar">
+                      {patient.first_name.charAt(0)}
+                      {patient.last_name.charAt(0)}
                     </div>
-                    <div className="meta-item">
-                      <Phone className="meta-icon" />
-                      <span>{patient.responsable_user.phone_number}</span>
-                    </div>
-                    <div className="meta-item">
-                      <Calendar className="meta-icon" />
-                      <span>
-                        Registrado:{" "}
-                        {new Date(patient.created_at).toLocaleDateString()}
-                      </span>
+
+                    <div className="patient-details">
+                      <h3 className="patient-name">
+                        {patient.first_name} {patient.last_name}
+                      </h3>
+                      <p className="patient-info">
+                        {calculateAge(patient.birth_date)} años •{" "}
+                        {patient.gender === "Masculino" ? "Niño" : "Niña"}
+                      </p>
+
+                      <div className="patient-meta">
+                        <div className="meta-item">
+                          <User className="meta-icon" />
+                          <span>
+                            {patient.responsable_user.first_name}{" "}
+                            {patient.responsable_user.last_name}
+                          </span>
+                        </div>
+                        <div className="meta-item">
+                          <Phone className="meta-icon" />
+                          <span>{patient.responsable_user.phone_number}</span>
+                        </div>
+                        <div className="meta-item">
+                          <Calendar className="meta-icon" />
+                          <span>
+                            Registrado:{" "}
+                            {new Date(patient.created_at).toLocaleDateString()}
+                          </span>
+                        </div>
+                      </div>
                     </div>
                   </div>
+                  <div className="patient-allergies">
+                    <span className="allergy-tag">⚠️ Alergias</span>
+                  </div>
                 </div>
-              </div>
-              <div className="patient-allergies">
-                <span className="allergy-tag">⚠️ Alergias</span>
-              </div>
-            </div>
-          ))}
+              ))}
+            </>
+          )}
         </div>
 
-        {filteredPatients.length === 0 && (
+        {filteredPatients.length === 0 && !waitingResponse && (
           <div className="no-patients">
             <User className="no-patients-icon" />
             <p className="no-patients-text">No se encontraron pacientes</p>
